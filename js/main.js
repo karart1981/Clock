2809cbd46c5d4b55b9d2aa26f5ae6b09a09a1264
art.karapetyan <karapart1981@gmail.com>
2022-09-09 18:04:24 +0400

    I created interactive clock plugin

---
+++ js/main.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+  window.setInterval(function() {
+    let date = new Date();
+
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	let seconds = date.getSeconds();
+    
+	if(hours < 10) hours = "0" + hours;
+	if(minutes < 10) minutes = "0" + minutes;
+	if(seconds < 10) seconds = "0" + seconds;
+	let clock = hours + ":" + minutes + ":" + seconds;
+	document.getElementById("clock").innerHTML = clock;
+  }, 1000);
+};
